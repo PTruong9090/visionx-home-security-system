0f95bda27685df8c7902658bca3ca92caa898cd3
Phuc Truong <PTruong9090@gmail.com>
2026-07-04 00:30:11 -0700

    added cors

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from src.routers import cameras
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="VisionX API")
+
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(cameras.router)
 
