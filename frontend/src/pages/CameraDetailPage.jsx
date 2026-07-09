832ee3849f0c9ef5c3b57910b7b6d42c5c476103
Phuc Truong <ptruong9090@gmail.com>
2026-07-08 18:36:25 -0700

    added test camera api and used delete hooked

--- frontend/src/pages/CameraDetailPage.jsx
+++ frontend/src/pages/CameraDetailPage.jsx
@@ -1,18 +1,42 @@
 import { getOneCamera } from "../api/cameraAPI"
 
-import { useParams } from "react-router-dom"
+import { useParams, NavLink, useNavigate } from "react-router-dom"
 import { useEffect, useState } from "react"
+
+import { testCamera } from "../api/cameraAPI"
 
 import EventsCard from "../components/cameraDetail/EventsCard"
 import OverviewCard from "../components/cameraDetail/OverviewCard"
 import HealthCard from "../components/cameraDetail/HealthCard"
 import InfoCard from "../components/cameraDetail/InfoCard"
 import CameraPlayer from "../components/cameraDetail/CameraPlayer"
+import CameraActionsMenu from "../components/cameras/CamerasActionsMenu"
+
+import useCameraDelete from "../hooks/useCameraDelete"
+import CameraDeleteModal from "../components/modals/CameraDeleteModal"
+
 
 export default function CameraDetailPage() {
+    const navigate = useNavigate()
+
     const [camera, setCamera] = useState(null)
     const { cameraId } = useParams()
     const [ activeTab, setActiveTab ] = useState("overview")
+    const [testing, setTesting] = useState(false)
+    const [testResult, setTestResult] = useState(null)
+
+    const {
+        cameraToDelete,
+        isDeleting,
+        requestDelete,
+        cancelDelete,
+        confirmDelete,
+    } = useCameraDelete({
+        onDeleted: () => {
+            navigate("/cameras")
+        },
+    })
+
 
     const isOnline = camera?.enabled
     const statusText = isOnline ? "Online" : "Offline"
@@ -21,6 +45,32 @@ export default function CameraDetailPage() {
 
     const isRecording = camera?.recording_enabled
 
+    async function testCameraConnection() {
+        setTesting(true)
+        setTestResult(null)
+
+        try {
+            const res = await testCamera(cameraId)
+
+            setTestResult({
+                success: true,
+                message: res.message || "Camera connection successful.",
+            })
+
+        } catch (error) {
+            console.error("Failed to test camera:", error)
+
+            setTestResult({
+                success: false,
+                message: "Failed to connect to camera.",
+            })
+        } finally {
+            setTesting(false)
+            setTimeout(() => {
+                setTestResult(null)
+            }, 3000)
+        }
+    }
 
     useEffect(() => {
         async function fetchCamera(cameraId) {
@@ -41,24 +91,27 @@ export default function CameraDetailPage() {
     }
 
     return (
-        <div className="mx-auto w-full max-w-5xl flex flex-col gap-4">
+        <div className="mx-auto flex min-h-[calc(100vh-8rem)] w-full max-w-5xl flex-col gap-4">
 
             <div className="flex justify-between">
                 <h2 className="text-xl font-semibold">{camera?.name}</h2>
-                <button>...</button>
+                <CameraActionsMenu 
+                    cameraId={cameraId}
+                    onDelete={() => requestDelete(camera)}
+                />
             </div>
 
             <div className="flex gap-3 items-center">
-                <div className="text-s text-[#CBD5E1]">{camera?.location}</div>
+                <div className="text-sm text-[#CBD5E1]">{camera?.location}</div>
 
-                <div className={`p-2 flex items-center gap-2 ${statusColor}`}>
-                    <div className={`h-3 w-3 rounded-full ${dotColor}`} />
-                    <p>{statusText}</p>
+                <div className={`p-2 flex items-center gap-1 ${statusColor}`}>
+                    <div className={`h-2 w-2 rounded-full ${dotColor}`} />
+                    <p className="text-sm">{statusText}</p>
                 </div>
                 
-                <div className="flex items-center gap-2">
-                    <div className={` ${isRecording ? "h-3 w-3 rounded-full bg-orange-400" : ""}`}></div>
-                    <div>{camera?.recording_enabled ? "Recording" : ""}</div>
+                <div className="flex items-center gap-1">
+                    <div className={` ${isRecording ? "h-2 w-2 rounded-full bg-orange-400" : ""}`}></div>
+                    <div className="text-sm">{isRecording ? "Recording" : ""}</div>
                 </div>
             </div>
 
@@ -87,14 +140,37 @@ export default function CameraDetailPage() {
             </div>
 
             {activeTab === "overview" && <OverviewCard camera={camera} />}
-            {activeTab === "info" && <div>Info card later</div>}
-            {activeTab === "health" && <div>Health card later</div>}
-            {activeTab === "events" && <div>Events card later</div>}
+            {activeTab === "info" && <InfoCard camera={camera} />}
+            {activeTab === "health" && <HealthCard camera={camera} />}
+            {activeTab === "events" && <EventsCard camera={camera} />}
 
-            <div className="flex gap-4 justify-end items-center m-4">
-                <button className="px-4 py-3 rounded-md bg-[#3B82F6] hover:bg-[#2563EB]">Test Connection</button>
-                <button className="px-4 py-3 rounded-md bg-[#111820] border border-[#24313C]">Edit Camera</button>
+            <div className="flex gap-4 justify-end items-center mt-auto">
+                {testResult && (
+                    <p className={`text-sm ${testResult.success ? "text-green-400" : "text-red-400"}`}>
+                        {testResult.message}
+                    </p>
+                )}
+
+                <div className="flex justify-end gap-4 items-center">
+                    <button 
+                        disabled={testing}
+                        onClick={testCameraConnection}
+                        className="px-4 py-3 rounded-md bg-[#3B82F6] hover:bg-[#2563EB]"
+                    >
+                            {testing ? "Testing..." : "Test Connection"}
+                    </button>
+
+                    <NavLink to={`/cameras/${cameraId}/edit`} className="px-4 py-3 rounded-md bg-[#111820] border border-[#24313C]">Edit Camera</NavLink>
+                </div>
             </div>
+
+            <CameraDeleteModal
+                camera={cameraToDelete}
+                isDeleting={isDeleting}
+                onCancel={cancelDelete}
+                onConfirm={confirmDelete}
+            />
+
         </div>
     )
 }
